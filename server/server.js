6780100451d108f965f43b0e8eb2f83441cc85e6
Kshitij Vijay Pawar <kshitij.vijay.pawar@gmail.com>
2025-10-23 10:11:04 +0530

    in server i add the backend url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,24 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "http://localhost:5174", 
+            "http://localhost:5175",
+            "https://authentication-system-frontend-d72x.onrender.com",
+            "https://authentication-system-5n81.onrender.com"
+        ];
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
